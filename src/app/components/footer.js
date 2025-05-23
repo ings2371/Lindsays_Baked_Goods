e340e1d264d6eb5c69fccf83bd95de75049ad14f
CurtisJLButler <curtisjlbutler@gmail.com>
2025-05-23 09:58:35 -0300

    Fixed header and footer in layout js

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,16 +2,12 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <footer className='w-full' style={{ flex: '1 1 45%', backgroundColor: 'black' }}>
-      <div className="container" style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-        <div>
-          <p style={{ textAlign: "left"}}>Contact info:</p>
-          <p style={{color: "white", textAlign: "left" }}>
+    <footer className='w-full' style={{backgroundColor: 'black' }}>
+        <div className='basis-full text-[white]'>
+          <p style={{ textAlign: "center"}}>Contact info:</p>
+          <p style={{ textAlign: "center" }}>
             lindsayings78@gmail.com
           </p>
-        </div>
-
-        
       </div>
   </footer>
   )
