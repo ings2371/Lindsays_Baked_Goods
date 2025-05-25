b6ff8e5c94f5d68e25db333d4247b6efedef7f30
W0468723 <W0468723@nscc.ca>
2025-05-25 13:43:47 -0300

    privacy done

--- src/app/Privacy/page.jsx
+++ src/app/Privacy/page.jsx
@@ -4,28 +4,28 @@ export default function Contact() {
 
 
     return (
-        <div>
-            <h1>Privacy Policy</h1>
-            <p>Last updated: May 23, 2025</p>
+        <div className="p-2">
+            <h1 className="text-center text-2xl">Privacy Policy</h1><br/>
+            <p>Last updated: May 23, 2025</p><br/>
 
-            <h2>who are we</h2>
-            <p>Our website is: lindsayssweettreats.com</p>
+            <h2 className="text-xl">who are we:</h2>
+            <p>Our website is: lindsayssweettreats.com</p><br/><br/>
 
-            <h2>Person data we collect and why</h2>
+            <h2 className="text-xl">Person data we collect and why:</h2>
             <p>we collect the following so if we need to contact you about any questions you have or relating to a order you did.</p>
-            <p>- Email address <br/>- Full name<br/>- City <br/>- comments</p>
+            <p>- Email address <br/>- Full name<br/>- City <br/>- comments</p><br/><br/>
 
-            <h2>How long we retain your data</h2>
-            <p>We keep your data indefinitely.</p>
+            <h2 className="text-xl">How long we retain your data:</h2>
+            <p>We keep your data indefinitely.</p><br/><br/>
 
-            <h2>How your data is stored</h2>
-            <p>It may be printed and stored in a archive.</p>
+            <h2 className="text-xl">How your data is stored:</h2>
+            <p>It may be printed and stored in a archive.</p><br/><br/>
 
-            <h2>Use of your personal data</h2>
+            <h2 className="text-xl">Use of your personal data:</h2>
             <p>We use your data so<br/>- we know who we are selling too amoung pick-up of whatever you order
-            <br/>- contact you about your question and contact you about the confirmation of your order</p>
+            <br/>- contact you about your question and contact you about the confirmation of your order</p><br/><br/>
 
-            <h2>Payments</h2>
+            <h2 className="text-xl">Payments:</h2>
             <p>Payments are done by e-Transfers or cash upon pick-up of the items you buy.<br/>
             We do NOT collect any card information of any kind at all.</p>
         </div>
