9a6ba3e666bfe7a580a04b1b5166a95bbb2debce
CurtisJLButler <curtisjlbutler@gmail.com>
2025-05-25 10:03:30 -0300

    Fixed card size

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -7,10 +7,12 @@ const Item = ({Baked_Good}) => {
             <Link href={`/baked-good-details/${Baked_Good._id}`}>
                 <div key={Baked_Good._id} className="flex items-center justify-center p-0 col-md-4">
                     <div className="object-right box-shadow">
-                        <img
-                        src={`/Baked_Goods/${Baked_Good.Thumbnail}`}
-                        className='w-18.5 h-26.5 md:h-53 md:w-37 lg:h-80 lg:w-56 lg:min-w-56'
-                        />
+                        <div className='w-13.25 h-13 md:h-26.5 md:w-37 lg:h-40 lg:w-56 lg:min-w-56 overflow-hidden'>
+                            <img
+                            src={`/Baked_Goods/${Baked_Good.Thumbnail}`}
+                            className='w-full h-full object-cover'
+                            />
+                        </div>
 
                         <p style={{textAlign: "center" }} className='w-18.5 md:w-37 lg:w-56 text-wrap text-sm md:text-lg lg:text-2xl' >
                             {Baked_Good.Baked_Name} <br/>
